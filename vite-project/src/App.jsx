e67f67997dc991ab479d8baae93dad338209d302
ntck <nontakorn.chatk@gmail.com>
2023-04-06 02:10:42 +0700

    First commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import Home from "./components/Home";
+import Todo from "./components/Todo";
+import Todopage from "./pages/Todopage";
+import Homepage from './pages/Homepage'
+import Todaypage from './pages/Todaypage'
+import Calendar from './pages/Calendar';
+
+import { Route, Routes } from 'react-router-dom';
+function App(){
+    return(
+        <Routes>
+        <Route exect path="/" element={<Homepage />}  />
+        <Route path="/todo" element={<Todopage />}  />
+        <Route path="/today" element={<Todaypage />}  />
+        <Route path="/calendar" element={<Calendar/>}  />
+      </Routes>
+    );
+}
+export default App;
